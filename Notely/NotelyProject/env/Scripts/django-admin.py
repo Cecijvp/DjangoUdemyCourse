e61d13ca78602bef51721c3eefe72896f476c943
Cecijvp <cecijvp.3@gmail.com>
2020-06-16 18:21:38 -0500

    Learning Django

---
+++ Notely/NotelyProject/env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\cecij\Documents\DjangoUdemyCourse\Notely\NotelyProject\env\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
