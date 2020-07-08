e88d0ea8b937c9072d71cae9ad35523cbe36b4d9
Cecijvp <cecijvp.3@gmail.com>
2020-07-08 17:14:16 -0500

    Learning Javascript

---
+++ JavaScript/Statements_Expressions.js
@@ -0,0 +1,52 @@
+20;
+20 + 30;
+
+//console.log(20+30);
+
+//expresión de strings
+
+'John is a boy';
+//console.log('Come Home')
+
+//Expresiones logicas 
+
+//console.log(5<10);
+//console.log(5>6);
+
+//expresiones de asignación
+var age = 16;
+
+//console.log(age);
+
+//Declaración de funciones:
+
+function getAge(age){
+ // console.log(age);
+}
+
+//getAge(15);
+
+//declaración condicional
+//if(age < 18){
+  //alert("Under Age Driver");
+//}else{
+  //console.log(age);
+//}
+
+//declaración de iteración:
+myArray =[];
+count=0;
+while(count<10){
+  myArray.push(count);
+  count++;
+}
+
+//console.log(myArray);
+
+//Funciones:
+
+function whatIsMyName(){
+  var name = 'Cecilia Vásquez';
+  alert(name);
+}
+whatIsMyName();
