509409d7f7e7a9cdbb9ab6165a10c2fee2f21a88
Cecijvp <cecijvp.3@gmail.com>
2020-07-15 17:50:12 -0500

    working with strings

---
+++ Python/PythonCrashCourse/PythonCrashCourse/string.py
@@ -0,0 +1,19 @@
+value1 = "When the year ends "
+value2 = "The last meal"
+
+result1 = value1 + value2 #Concatenar
+print(result1)
+
+result2 = value1 * 2 #repetir
+print(result2)
+
+result3 = value1[5]  #regresa la letra en esa posición
+print(result3)
+
+result4 = value2[1:5] #regresa todas las letras que hay en esas posiciones
+print(result4)
+
+result5 = value1.casefold() #convierte mayusculas en minusculas
+print(result5)
+result6 = value2.count('a') #regresa las veces que ese char está en el string
+print(result6)
