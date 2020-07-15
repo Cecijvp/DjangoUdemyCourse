5e8ae9a5650704d27d4b32ac7d5392dc9f5d7c74
Cecijvp <cecijvp.3@gmail.com>
2020-07-15 17:31:44 -0500

    numbers and math

---
+++ Python/PythonCrashCourse/PythonCrashCourse/numbers.py
@@ -0,0 +1,10 @@
+import math as mathModule
+
+value1 = 10
+value2 = 4
+result = pow(value1,value2)
+result2 = mathModule.sqrt(value2)
+result3 = mathModule.floor(value1)
+print(result)
+print(result2)
+print(result3)
