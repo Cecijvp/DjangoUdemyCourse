57c95f46acd7af5a1851067540d84f65b5c27974
Cecijvp <cecijvp.3@gmail.com>
2020-07-15 18:07:35 -0500

    working with boolean

---
+++ Python/PythonCrashCourse/PythonCrashCourse/boolean.py
@@ -0,0 +1,17 @@
+value1 = True
+value2 = False
+
+var1 = 10
+var2 = 20
+
+bool1= var1 >var2
+bool2 = (var2-var1) < var2
+print(bool1)
+print(bool2)
+
+result1 = bool1 and bool2 #AND Operation
+result2 = bool1 or bool2
+result3 = not bool1
+print(result1)
+print(result2)
+print(result3)
