5e427526d37af56d9eb661743ca63ae77121df91
Cecijvp <cecijvp.3@gmail.com>
2020-07-16 18:12:27 -0500

    operators

---
+++ Python/PythonCrashCourse/PythonCrashCourse/operators.py
@@ -0,0 +1,31 @@
+#Arithmetic operators
+
+x=20
+y=5
+
+result = x+y 
+result1= x-y
+result2= x*y
+result3= x/y
+result4= x%y
+print(result)
+print(result1)
+print(result2)
+print(result3)
+print(result4)
+
+#Assigment operators
+z= x +y
+print(z)
+z+=y
+print(z)
+
+#comparison 
+
+print(x<y)
+print(x==y)
+print(x>y)
+
+#logical
+
+print((x>y) and (x==y))
