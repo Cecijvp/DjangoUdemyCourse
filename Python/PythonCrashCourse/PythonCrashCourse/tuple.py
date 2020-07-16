93b78c6849434469b96413246e2b9ecd8c886750
Cecijvp <cecijvp.3@gmail.com>
2020-07-16 17:45:33 -0500

    learning tuples

---
+++ Python/PythonCrashCourse/PythonCrashCourse/tuple.py
@@ -0,0 +1,12 @@
+#Tuple dataType
+
+myTuple = ("james","john","peter","Jane")
+
+result = myTuple[1]
+print(result)
+result1 = len(myTuple)
+print(result1)
+
+#looping
+for t in myTuple:
+    print(t)
